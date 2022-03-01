2f4ef4cb7c104477f97310de9c6a864ae3905ba3
Junseo Kang <kji98765@naver.com>
2022-03-01 14:43:23 +0900

    Add files via upload

---
+++ toy-web/app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+app.listen(3000, function() {
+	console.log("start! express server on port 3000");
+});
+
+app.use(express.static('public'))
+
+//routing
+app.get('/', function(req,res){
+	res.sendFile(__dirname + "/public/main.html")
+})
+
+app.get('/main', function (req, res) {
+	res.sendFile(__dirname + "/public/main.html")
+})
