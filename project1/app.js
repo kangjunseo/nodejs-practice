a51c1dbdbb71e663407cc29b6eca72352f4e65bf
kangjunseo <kji98765@naver.com>
2022-03-02 22:00:26 +0900

    project1

---
+++ project1/app.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+app.listen(3000, function() {
+	console.log("start! express server on port 3000");
+});
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
+
+//routing
+app.get('/', function(req,res){
+	res.sendFile(__dirname + "/public/main.html")
+})
+
+app.get('/main', function (req, res) {
+	res.sendFile(__dirname + "/public/main.html")
+})
+
+app.get('/project1', function (req, res) {
+	res.sendFile(__dirname + "/public/form.html")
+})
+
+app.post('/email_post', function(req,res){
+	console.log(req.body.email)
+	//res.send("<h1>welcome "+req.body.email+"</h1>")
+	res.render('email.ejs', {'email' : req.body.email})
+})
+
+app.post('/ajax_send_email', function(req,res){
+	var responseData = {'result' : 'ok', 'email' : req.body.email};
+	res.json(responseData);
+})
