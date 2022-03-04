cfdd8f25b5e92cd382cd993e6e5b641e9216ffa6
kangjunseo <kji98765@naver.com>
2022-03-05 01:58:59 +0900

    6 start

---
+++ 6. passport_auth/router/index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+var router = express.Router();
+var path = require('path')
+var main = require('./main/main')
+var email = require('./email/email')
+var join = require('./join/index')
+
+//routing
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/main.html"))
+})
+router.use('/main', main)
+router.use('/email', email)
+router.use('/join',join)
+
+module.exports = router;
